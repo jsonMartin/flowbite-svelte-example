8c4455f0060f1e15ed7246f37c08239dc8220c1a
jsonMartin <4033621+jsonMartin@users.noreply.github.com>
2023-08-02 16:55:08 -0700

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { Button } from 'flowbite-svelte';
+</script>
+
+<Button>Click me</Button>
+<h1>Gallery example</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
